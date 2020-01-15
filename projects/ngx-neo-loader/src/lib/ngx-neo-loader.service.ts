4f11a11ab58548028dbc871ea51715fe817c1937
Sebastian Larrieu <slarrieu@team.neocomplexx.com>
2020-01-15 08:58:01 -0300

    Acces subscription

--- projects/ngx-neo-loader/src/lib/ngx-neo-loader.service.ts
+++ projects/ngx-neo-loader/src/lib/ngx-neo-loader.service.ts
@@ -10,18 +10,25 @@ export class NgxNeoLoaderService {
 
     loaderState = this.loaderSubject.asObservable();
 
+    private sub: Subscription;
+
     constructor() { }
 
     public show(): Subscription {
         const timerObs = timer(this.DELAY_TIME);
         // subscribing to a observable returns a subscription object
-        const sub = timerObs.subscribe(t => this.loaderSubject.next(<LoaderState>{showLoading: true}));
+        const sub = timerObs.subscribe(t => this.loaderSubject.next(<LoaderState>{ showLoading: true }));
+        this.sub = sub;
         return sub;
         // this.loaderSubject.next(<LoaderState>{showLoading: true});
     }
 
-    public hide(sub: Subscription) {
-        sub.unsubscribe();
-        this.loaderSubject.next(<LoaderState>{showLoading: false});
+    public hide(sub: Subscription = null) {
+        if (!sub && this.sub) {
+            this.sub.unsubscribe();
+        } else if (sub) {
+            sub.unsubscribe();
+        }
+        this.loaderSubject.next(<LoaderState>{ showLoading: false });
     }
 }
